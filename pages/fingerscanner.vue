a8d0f94e50ad348164052aaa5e5b9b6c154449eb
pythonvista <emeterv18@gmail.com>
2023-08-18 08:33:47 +0100

    updated

--- pages/fingerscanner.vue
+++ pages/fingerscanner.vue
@@ -43,7 +43,7 @@ export default {
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(attResp),
+          body: JSON.stringify({ ...attResp, currentChallenge: opts.challenge }),
         }
       );
 
